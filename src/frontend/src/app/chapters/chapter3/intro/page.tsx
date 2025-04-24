2cd2b27480505cd130aca6976a4cc4f67a3faba3
KateChuu <mabo2918@gmail.com>
2025-04-24 16:41:37 +1000

    fix section icons

--- src/frontend/src/app/chapters/chapter3/intro/page.tsx
+++ src/frontend/src/app/chapters/chapter3/intro/page.tsx
@@ -9,17 +9,24 @@ export default function page() {
             nextHref="/chapters/chapter3/section1"
             chapterHref="/chapters/chapter3"
         >
-            {/* Image 1 */}
+            {/* Fish Image */}
             <div className='my-4 flex justify-center'>
-                First image here
+                <img 
+                    src="https://res.cloudinary.com/difs4tswt/image/upload/v1745114785/how-finding-nemo-works-5-26_b6y2t8.jpg"
+                    alt="Fish image" 
+                    className="my-4 w-1/2 mx-auto" 
+                />
             </div>
 
-            {/* Image 2 */}
+            {/* Dentist Image */}
             <div className='my-4 flex justify-center'>
-                Second image here
+                <img 
+                    src="https://res.cloudinary.com/difs4tswt/image/upload/v1745114788/FindingNemo2003720pHDRiP-4-24_ftcr58.jpg"
+                    alt="Dentist image" 
+                    className="my-4 w-1/2 mx-auto" 
+                />
             </div>
 
-           
 
         </SectionPage>
     );
