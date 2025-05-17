573a5290d6f31dae79e7963cf2c27cdf3fe147dc
icredback <icredback@gmail.com>
2025-05-18 05:00:13 +1000

    modify error

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,20 +1,23 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './src/**/*.{js,ts,jsx,tsx}',
+        "./src/app/**/*.{js,ts,jsx,tsx}",    
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.css",
     ],
+    darkMode: 'class',
     theme: {
         extend: {
+            animation: {
+                slide: 'slide 30s linear infinite',
+            },
             keyframes: {
-                'infinite-scroll': {
-                    '0%': { transform: 'translateX(0)' },
+                slide: {
+                    '0%': { transform: 'translateX(0%)' },
                     '100%': { transform: 'translateX(-50%)' },
                 },
-            },
-            animation: {
-                'infinite-scroll': 'infinite-scroll 40s linear infinite',
             },
         },
     },
     plugins: [],
-};
+}
