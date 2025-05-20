ccacc3756875af7784435d686c034587f758a929
icredback <icredback@gmail.com>
2025-05-21 02:50:55 +1000

    Modify home layout

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -99,7 +99,7 @@ export default function ChapterCarousel() {
     return (
         <div className="flex flex-col items-center">
             {/* Carousel */}
-            <div id="carousel-example" className="relative w-full max-w-5xl mx-auto px-4 md:px-8 py-6" data-carousel="slide">
+            <div id="carousel-example" className="relative w-full max-w-screen-xl mx-auto px-4 py-10" data-carousel="slide">
                 {/* Slide images */}
                 <div className="relative h-64 overflow-hidden rounded-lg md:h-96">
                     {images.map((id, idx) => (
@@ -120,7 +120,7 @@ export default function ChapterCarousel() {
                 </div>
 
                 {/* Indicators */}
-                <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3">
+                <div className="absolute z-30 flex -translate-x-1/2 bottom-12 left-1/2 space-x-3">
                     {images.map((_, idx) => (
                         <button
                             key={idx}
